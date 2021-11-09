247956cb2e3f1999640f68fd0921efe2d8bde548
DefyCab <love.bellinder@gmail.com>
2021-11-09 23:17:12 +0100

    ny gren

--- src/script.js
+++ src/script.js
@@ -1,10 +1,33 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 
+const shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-startButton.classList.add('hide')
-questionContainerElement.classList.remove('hide')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion(){
 
 }
+
+function selectAnswer(){
+
+}
+
+const question = [
+    {
+       question: 'Vad heter Rumäniens huvudstad',
+       answers: [
+           { text: 'Bukarest', correct: true},
+           { text: 'Sofia', correct: false},
+           { text: 'Köpenhamn', correct: false},
+           { text: 'Budapest', correct: false}
+
+       ] 
+    }
+]
